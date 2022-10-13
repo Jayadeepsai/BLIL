372195994e93a37e09087b7be69ffc3ed5fb5691
Jayadeepsai <jayadeepsaim@gmail.com>
2022-10-13 11:40:42 +0530

    pushed

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const http = require('http');
 const app = require('c:/Users/jayad/Desktop/Login nodejs/app/app')
 // Create an instance of the http server to handle HTTP requests
-const port =process.env.PORT || 8000;
+const port =process.env.PORT || 5000;
 
 const server = http.createServer(app);
 // Start the server on port 8000
