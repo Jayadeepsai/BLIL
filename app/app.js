5b06698e5f5bf03f900d2d9938fcc7d34a34f16c
Jayadeepsai <jayadeepsaim@gmail.com>
2022-10-13 11:34:15 +0530

    changed all paths

--- app/app.js
+++ app/app.js
@@ -5,12 +5,22 @@ const bodyParser = require('body-parser');
 const session = require('express-session');
 const app = express();
 
-const userprofile=require('../app/api/userprofile');
-const signup=require('../app/api/signup');
+const userprofile=require('c:/Users/jayad/Desktop/Login nodejs/app/api/userprofile');
+const signup=require('c:/Users/jayad/Desktop/Login nodejs/app/api/signup');
 //const validation=require('./validation');
-const router=require('../app/api/router');
-const indexRouter = require('../app/api/router');
+const router=require('c:/Users/jayad/Desktop/Login nodejs/app/api/router');
+const indexRouter = require('c:/Users/jayad/Desktop/Login nodejs/app/api/router');
 
+/*app.use((req,res,next)=>{
+    
+    res.header('Access-Control-Allow-Origin','*');
+    res.header('Access-Control-Allow-Header','*');
+    if(req.method =='OPTIONS'){
+        res.header('Access-Control-Allow-Origin','PUT,POST,PATCH,DELETE,GET');
+        return res.status(200).json({});
+    }
+    next();
+})*/
 
 app.use(flash());
 app.use(bodyParser.json());
